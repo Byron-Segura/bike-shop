2e52ce86d2c260d288d577d4782a44f6448a115d
Byron SeMo <byronsemo@gmail.com>
2023-08-17 19:07:21 -0700

    Change header styles and Add NavLinks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,22 @@ import { NavLink } from './NavLink'
 
 export function Header () {
   return (
-    <header className='min-w-full sticky top-0 bg-gray-50'>
-      <div className='flex flex-col align-middle md:flex-row md:justify-between h-18 min-w-full'>
-        <h2 className='text-4xl w-fit m-auto md:m-0 p-4'>Bike40</h2>
-        <nav>
-          <ul className='flex flex-row h-10 md:min-h-full bg-gray-100 min-w-full mx-auto'>
-            <NavLink style='text-base p-2 sm:text-lg md:text-xl md:p-6 px-6 bg-gray-100 hover:bg-gray-300 md:shadow-[-8px_0_8px_-5px_rgba(0,0,0,0.1)]' to='/choose-bike'>Choose a bike</NavLink>
-            <NavLink to='/'>Bikes</NavLink>
-            <NavLink>About Us</NavLink>
+    <header className='min-w-full min-h-fit sticky top-0 bg-gray-900'>
+      <nav className='flex flex-col md:flex-row md:justify-between h-18 min-w-full'>
+        <NavLink to='/' style='flex flex-col justify-center text-4xl w-fit m-auto md:m-0 p-4 text-pink-600 md:px-8'>
+          BikeBuy
+          <small className='text-xs font-normal text-slate-200 '>
+            ONLY THE BEST BIKES
+          </small>
+        </NavLink>
+        <div className='flex flex-row items-center justify-center bg-gray-800/40 md:min-h-full'>
+          <ul className='text-center grid grid-cols-3 place-content-center gap-4 p-2 px-4 '>
+            <NavLink to='all-bikes'>Bikes</NavLink>
+            <NavLink to='choose-bike'>Choose Bike</NavLink>
+            <NavLink to='about-us'>About Us</NavLink>
           </ul>
-        </nav>
-      </div>
+        </div>
+      </nav>
     </header>
   )
 }
