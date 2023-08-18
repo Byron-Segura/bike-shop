621a9f618bffcce9a923e35ec9e61ee5889dda04
Byron SeMo <byronsemo@gmail.com>
2023-08-17 19:06:42 -0700

    Add routes

---
+++ src/components/Main.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
+import { HomePage } from '../pages/Home'
+import { ChooseBike } from '../pages/ChooseBike'
+import { About } from '../pages/About'
+import { Bikes } from '../pages/Bikes'
+
+export function Main () {
+  return (
+    <main className='bg-slate-50 min-h-screen'>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='choose-bike' element={<ChooseBike />} />
+        <Route path='about-us' element={<About />} />
+        <Route path='all-bikes' element={<Bikes />} />
+      </Routes>
+    </main>
+  )
+}
