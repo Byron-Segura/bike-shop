9dd00819f2d32b04463355eb06f2951ede0e1df7
Byron SeMo <byronsemo@gmail.com>
2023-08-24 21:18:05 -0700

    Add Products.jsx

---
+++ src/components/Products.jsx
@@ -0,0 +1,18 @@
+export function Products (products) {
+  console.log(products)
+  return (
+    <>
+      <ul className='flex flex-col md:flex-row py-10 gap-8 items-center justify-center'>
+        {products.products.map(product => {
+          return (
+            <li key={product.id} className='max-w-sm cursor-pointer hover:scale-105 transition-transform duration-150'>
+              <img src={`../../src/assets/${product.image}.jpg`} alt={`Bicycle image, ${product.name}`} className='h-56' />
+              <h2 className='text-xl px-2 mt-8'>{product.name}</h2>
+              <h4 className='text-lg text-red-600 font-light px-2'>{product.price}</h4>
+            </li>
+          )
+        })}
+      </ul>
+    </>
+  )
+}
