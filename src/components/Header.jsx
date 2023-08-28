264d5052ee3f974a3bc218fdbd418f78923435e2
Byron SeMo <byronsemo@gmail.com>
2023-08-27 23:22:47 -0700

    Change Header.jsx style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import { NavLink } from './NavLink'
 
 export function Header () {
   return (
-    <header className='min-w-full min-h-fit sticky top-0 bg-gray-900'>
-      <nav className='flex flex-col md:flex-row md:justify-between h-18 min-w-full'>
+    <header className='min-w-full min-h-fit relative top-0 bg-gray-900 z-50'>
+      <nav className='flex text- flex-col md:flex-row md:justify-between h-18 min-w-full'>
         <NavLink to='/' style='flex flex-col justify-center text-4xl w-fit m-auto md:m-0 p-4 text-pink-600 md:px-8'>
           BikeBuy
           <small className='text-xs font-normal text-slate-200 '>
@@ -11,10 +11,11 @@ export function Header () {
           </small>
         </NavLink>
         <div className='flex flex-row items-center justify-center bg-gray-800/40 md:min-h-full'>
-          <ul className='text-center grid grid-cols-3 place-content-center gap-4 p-2 px-4 '>
-            <NavLink to='all-bikes'>Bikes</NavLink>
+          <ul className='text-center grid grid-cols-3 auto-cols-auto sm:grid-cols-4 place-content-center gap-2 md:gap-4 p-2 px-4 '>
+            <NavLink to='all-bikes'>Bicycles</NavLink>
             <NavLink to='choose-bike'>Choose Bike</NavLink>
-            <NavLink to='about-us'>About Us</NavLink>
+            <NavLink to='choose-bike'>Gear</NavLink>
+            <NavLink to='about-us' style='text-base md:text-lg md:hover:text-pink-600 text-slate-200 transition-colors font-medium col-start-2 sm:col-start-auto'>About Us</NavLink>
           </ul>
         </div>
       </nav>
