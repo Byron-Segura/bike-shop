6f12fea6ab189cd1368b8b51e078314078a275a0
Byron SeMo <byronsemo@gmail.com>
2023-08-29 20:55:31 -0700

    Change PopularProducts.jsx styles

--- src/pages/home/PopularProducts.jsx
+++ src/pages/home/PopularProducts.jsx
@@ -1,14 +1,14 @@
-import { ProductCard } from '../../components/Products'
+import { Products } from '../../components/Products'
 import bikes from '../../mocks/bikes.json'
 
 export function PopularProducts () {
   const products = [bikes.bikes[9], bikes.bikes[10], bikes.bikes[3]]
 
   return (
-    <section className='bg-white min-h-screen'>
-      <article className='p-4 py-16 flex flex-col justify-center items-center'>
-        <h2 className='text-7xl text-gray-800'>Best Sellers</h2>
-        <ProductCard products={products} />
+    <section className='bg-white min-h-screen flex flex-col justify-center items-center'>
+      <article className='p-4 pt-24 pb-8 flex flex-col justify-center items-center max-w-7xl'>
+        <h2 className='text-5xl md:text-7xl text-gray-800'>Best Sellers</h2>
+        <Products products={products} />
       </article>
     </section>
   )
