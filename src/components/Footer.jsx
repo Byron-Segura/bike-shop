fa146e25b5dbc8e1f3498f766b9b2c3d6639c4ac
Byron SeMo <byronsemo@gmail.com>
2023-08-29 20:17:56 -0700

    Add Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export function Footer () {
+  return (
+    <section className='bg-gray-900'>
+      <article className='p-4'>
+        <h2>Contact Us</h2>
+      </article>
+    </section>
+  )
+}
