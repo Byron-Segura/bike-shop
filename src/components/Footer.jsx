9b2ac3d5710cd4be6a3f56771beffa40af5acad8
Byron SeMo <byronsemo@gmail.com>
2023-08-29 20:54:18 -0700

    Change Footer.jsx content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,31 @@
 export function Footer () {
   return (
-    <section className='bg-gray-900'>
-      <article className='p-4'>
-        <h2>Contact Us</h2>
+    <section className='bg-zinc-900 h-fit md:h-[60vh]'>
+      <article className='p-4 h-4/5 md:p-16'>
+        <ul className='text-slate-200 flex flex-col md:flex-row gap-8 items-center justify-around'>
+          <li>
+            <h2 className='border-b-2 border-b-zinc-800 text-2xl py-2'>Contact Us</h2>
+            <div className='text-zinc-300 flex flex-col gap-4 pt-2'>
+              <h3>Email:</h3>
+              <h4>bikebuy@gmail.com</h4>
+              <h3>Celphone:</h3>
+              <h4>1111-1111</h4>
+              <h3>Location:</h3>
+              <h4>Cartago, Costa Rica</h4>
+            </div>
+          </li>
+          <li>
+            <h2 className='border-b-2 border-b-zinc-800 text-2xl py-2'>Social Media</h2>
+            <div className='text-zinc-300 flex flex-col gap-4 pt-2'>
+              <h3>Email:</h3>
+              <h4>bikebuy@gmail.com</h4>
+              <h3>Celphone:</h3>
+              <h4>1111-1111</h4>
+              <h3>Location:</h3>
+              <h4>Cartago, Costa Rica</h4>
+            </div>
+          </li>
+        </ul>
       </article>
     </section>
   )
