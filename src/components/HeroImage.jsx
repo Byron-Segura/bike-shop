3970e643b2acb7c446327da640a8edaed9cedcf7
Byron SeMo <byronsemo@gmail.com>
2023-08-31 16:44:09 -0700

    Add HeroImage.jsx

---
+++ src/components/HeroImage.jsx
@@ -0,0 +1,12 @@
+export function HeroImage ({ children, image, styles }) {
+  // const heroStyle = styles || `min-h-screen opacity-90 bg-[url("../src/assets/${image}")] bg-cover bg-center top-0 left-0 -mt-16`
+  return (
+    <>
+      <article className={`h-[45vh] sm:min-h-[70vh] min-w-full opacity-90 bg-[url("../src/assets/${image}")] bg-cover bg-center-top top-0 left-0`}>
+        <aside className='bg-black/40 h-[40vh] md:min-h-[70vh] text-gray-50 p-4 md:p-8'>
+          {children}
+        </aside>
+      </article>
+    </>
+  )
+}
