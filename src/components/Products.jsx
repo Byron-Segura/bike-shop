8fb8c672732360082638ecc95652a2aaf92051db
Byron SeMo <byronsemo@gmail.com>
2023-09-01 17:23:51 -0700

    Change Products.jsx layout

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,11 +1,11 @@
-export function Products (products) {
+export function Products ({ products, layout }) {
   return (
     <>
-      <ul className='flex flex-col lg:flex-row py-10 gap-8 items-center justify-center'>
-        {products.products.map(product => {
+      <ul className={layout}>
+        {products.map(product => {
           return (
             <li key={product.id} className='max-w-sm cursor-pointer '>
-              <img src={`../../src/assets/${product.image}.jpg`} alt={`Bicycle image, ${product.name}`} className='h-56 hover:scale-105 transition-transform duration-300' />
+              <img src={`../../src/assets/${product.image}.jpg`} alt={`Bicycle, ${product.name}`} className='h-56 hover:scale-105 transition-transform duration-300' />
               <h2 className='text-xl px-2 mt-8'>{product.name}</h2>
               <h4 className='text-lg text-orange-500 px-2'>{product.price}</h4>
             </li>
@@ -15,3 +15,5 @@ export function Products (products) {
     </>
   )
 }
+
+// flex flex-col lg:flex-row py-10 gap-8 items-center justify-center
--- src/pages/home/PopularProducts.jsx
+++ src/pages/home/PopularProducts.jsx
@@ -8,7 +8,7 @@ export function PopularProducts () {
     <section className='bg-white min-h-screen flex flex-col justify-center items-center'>
       <article className='p-4 pt-24 pb-8 flex flex-col justify-center items-center max-w-7xl'>
         <h2 className='text-5xl md:text-7xl text-slate-800'>Best Sellers</h2>
-        <Products products={products} />
+        <Products products={products} layout='flex flex-col lg:flex-row py-10 gap-8 items-center justify-center' />
       </article>
     </section>
   )
