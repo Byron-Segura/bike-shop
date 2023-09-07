30da39b259eefadd4251d2f24d4c79b06e8aaa36
Byron SeMo <byronsemo@gmail.com>
2023-09-06 18:00:37 -0700

    Change Categories.jsx style

--- src/pages/bikes/Categories.jsx
+++ src/pages/bikes/Categories.jsx
@@ -1,15 +1,17 @@
 import { NavLink } from '../../components/NavLink'
 
 export function Categories () {
+  const categorieNavLinkStyle = 'text-zinc-800 border-b-2 border-zinc-500 hover:border-b-2 hover:border-orange-500 w-full pt-4'
+
   return (
     <article className='w-fit p-4 md:px-16'>
-      <button className='text-orange-500 text-xl text-center border-[1px] border-orange-500 py-2 px-24 hover:bg-orange-500 hover:text-slate-200'>Categories</button>
-      <ul className='flex  flex-row md:flex-col gap-4 mt-2'>
-        <NavLink styles='text-zinc-800 border-b-2 border-zinc-500 hover:border-b-2 hover:border-orange-500 w-full pt-4'>Road</NavLink>
-        <NavLink styles='text-zinc-800 border-b-2 border-zinc-500 hover:border-b-2 hover:border-orange-500 w-full pt-4'>Urban</NavLink>
-        <NavLink styles='text-zinc-800 border-b-2 border-zinc-500 hover:border-b-2 hover:border-orange-500 w-full pt-4'>Gravel</NavLink>
-        <NavLink styles='text-zinc-800 border-b-2 border-zinc-500 hover:border-b-2 hover:border-orange-500 w-full pt-4'>Mountain</NavLink>
-        <NavLink styles='text-zinc-800 border-b-2 border-zinc-500 hover:border-b-2 hover:border-orange-500 w-full pt-4'>Active</NavLink>
+      <ul className='flex flex-row md:flex-col gap-4 mt-2 sticky top-8'>
+        <button className='text-orange-500 text-xl text-center border-[1px] border-orange-500 py-2 px-24 hover:bg-orange-500 hover:text-white'>Categories</button>
+        <NavLink styles={categorieNavLinkStyle}>Road</NavLink>
+        <NavLink styles={categorieNavLinkStyle}>Urban</NavLink>
+        <NavLink styles={categorieNavLinkStyle}>Gravel</NavLink>
+        <NavLink styles={categorieNavLinkStyle}>Mountain</NavLink>
+        <NavLink styles={categorieNavLinkStyle}>Active</NavLink>
       </ul>
     </article>
   )
