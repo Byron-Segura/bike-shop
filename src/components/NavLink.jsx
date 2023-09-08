9ec2e25df50000b9ddbf9bd5c715a1261bd08dd3
Byron SeMo <byronsemo@gmail.com>
2023-09-07 17:19:05 -0700

    Change NavLink.jsx style

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -1,7 +1,7 @@
 import { NavLink as NavLinkReactRouter } from 'react-router-dom'
 
 export function NavLink ({ to, children, styles, ...props }) {
-  const navStyle = styles || 'text-base md:text-md md:hover:text-orange-500 text-slate-200 transition-colors font-medium'
+  const navStyle = styles || 'text-base md:text-md md:hover:text-orange-500 text-slate-200 transition-colors'
 
   return (
     <NavLinkReactRouter
