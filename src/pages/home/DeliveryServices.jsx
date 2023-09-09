a73e6885c02b72e83eba3c0997ebc1268b76de5d
Byron SeMo <byronsemo@gmail.com>
2023-09-08 20:01:16 -0700

    Change DeliveryServices.jsx paragraph

--- src/pages/home/DeliveryServices.jsx
+++ src/pages/home/DeliveryServices.jsx
@@ -4,7 +4,8 @@ export function DeliveryServices () {
       <article className='h-2/3 p-6 md:p-10 flex flex-col-reverse md:flex-row gap-2 md:gap-12 font-light items-center'>
         <div className='text-slate-200 p-4 py-8'>
           <h2 className='text-4xl font-normal pb-4 text-orange-500'>The best service</h2>
-          <p className='text-lg'>All the products in this website are ready for delivery. The delivery service is available worldwide. Free delivery in orders above $100 and it will be delivered in a period of 1 or 2 weeks after order the products.</p>
+          <p className='text-lg'>All the products in this website are ready for delivery. The delivery service is available worldwide.</p>
+          <p>Free delivery in orders above $100 and it will be delivered in a period of 1 or 2 weeks after order the products.</p>
           <ul className='text-slate-200 py-8 flex flex-row gap-8 text-md sm:text-lg md:text-xl justify-center lg:justify-start'>
             <li className='flex flex-row gap-2 items-center'>
               <img src='../src/assets/delivery-icon.svg' alt='Fast delivery icon' />
