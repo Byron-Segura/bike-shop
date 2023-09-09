aea698f45c1765cb60a83f5235916b58c780bfa8
Byron SeMo <byronsemo@gmail.com>
2023-09-08 20:01:47 -0700

    Change HeroImage.jsx style

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -2,7 +2,7 @@ export function HeroImage ({ children, image }) {
   return (
     <>
       <article className={`h-[45vh] sm:min-h-[70vh] min-w-full opacity-90 bg-[url("${image}")] bg-cover bg-center-top top-0 left-0`}>
-        <aside className='bg-black/40 h-[40vh] md:min-h-[70vh] text-gray-50 p-4 md:p-8'>
+        <aside className='bg-black/40 h-full text-gray-50 p-4 md:p-8'>
           {children}
         </aside>
       </article>
