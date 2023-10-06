c6278fade49ca00d9eed773ee3c0a82416c631c6
Byron SeMo <byronsemo@gmail.com>
2023-10-06 15:26:47 -0700

    Change HeroImage.jsx image

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -1,7 +1,8 @@
-export function HeroImage ({ children, image }) {
+export const HeroImage = ({ children, image }) => {
+  const heroImage = image
   return (
     <>
-      <article className={`h-[45vh] sm:min-h-[70vh] min-w-full opacity-90 bg-[url("${image}")] bg-cover bg-center-top top-0 left-0`}>
+      <article className={'h-[45vh] sm:min-h-[70vh] min-w-full opacity-90")] bg-cover bg-center-top top-0 left-0'} style={{ backgroundImage: `url("${heroImage}")` }}>
         <aside className='bg-black/40 h-full text-gray-50 p-4 md:p-8'>
           {children}
         </aside>
