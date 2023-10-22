e7486afbf8a757ad3d3ab28225f05ed4565c859d
Byron SeMo <byronsemo@gmail.com>
2023-10-21 17:49:11 -0700

    Change Categories.jsx style

--- src/pages/bikes/Categories.jsx
+++ src/pages/bikes/Categories.jsx
@@ -4,22 +4,27 @@ import { NavLink } from '../../components/NavLink'
 export function Categories () {
   const [categorieIsActive, setCategorieIsActive] = useState(false)
 
-  const categorieClick = () => {
+  const handleClick = () => {
     setCategorieIsActive(!categorieIsActive)
   }
 
-  const categorieNavLinkStyle = categorieIsActive ? 'text-zinc-800 border-b-2 border-zinc-500 hover:border-b-2 hover:border-orange-500 w-full pt-4' : 'hidden'
+  const categorieNavLinkStyle = categorieIsActive ? 'text-zinc-800 text-xl border-b-2 border-orange-500 lg:hover:border-b-2 lg:hover:border-orange-500 w-full py-4' : 'hidden'
 
   return (
-    <article className='w-fit p-4 md:px-16'>
-      <ul className='flex flex-row md:flex-col gap-4 mt-2 sticky top-8'>
-        <button onClick={() => categorieClick()} className='text-orange-500 text-xl text-center border-[1px] border-orange-500 py-2 px-24 hover:bg-orange-500 hover:text-white'>Categories</button>
-        <NavLink styles={categorieNavLinkStyle}>Road</NavLink>
-        <NavLink styles={categorieNavLinkStyle}>Urban</NavLink>
-        <NavLink styles={categorieNavLinkStyle}>Gravel</NavLink>
-        <NavLink styles={categorieNavLinkStyle}>Mountain</NavLink>
-        <NavLink styles={categorieNavLinkStyle}>Active</NavLink>
+    <article className='w-fit md:px-16 mb-8 mx-auto md:mx-0'>
+      <ul className='flex flex-col mt-2 sticky top-8'>
+        <button onClick={handleClick} className='text-orange-500 text-xl text-center border-[1px] border-orange-500 py-2 px-20 sm:px-24 lg:hover:bg-orange-500 lg:hover:text-white'>Categories</button>
+        <NavLink to='road' styles={categorieNavLinkStyle}>Road</NavLink>
+        <NavLink to='urban' styles={categorieNavLinkStyle}>Urban</NavLink>
+        <NavLink to='gravel' styles={categorieNavLinkStyle}>Gravel</NavLink>
+        <NavLink to='mountain' styles={categorieNavLinkStyle}>Mountain</NavLink>
+        <NavLink to='active' styles={categorieNavLinkStyle}>Active</NavLink>
       </ul>
     </article>
   )
 }
+
+/*
+  send wich nav was clicked and then filter
+  the products by bike type and change the products state
+*/
