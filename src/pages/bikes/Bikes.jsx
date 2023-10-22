653f8d9c17bcaea212ecbe66d4770fd7bf0eefc0
Byron SeMo <byronsemo@gmail.com>
2023-10-21 17:47:26 -0700

    Change Bikes.jsx interface

--- src/pages/bikes/Bikes.jsx
+++ src/pages/bikes/Bikes.jsx
@@ -1,36 +1,40 @@
-import { HeroImage } from '../../components/HeroImage'
+import { HeroImage } from '../../components/HeroImage.jsx'
 import { Categories } from './Categories'
+import bikes from '../../mocks/bikes.json'
+import { Products } from '../../components/Products'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 export function Bikes () {
+  const allBikes = bikes.bikes
+  const [products, setProducts] = useState(allBikes)
+  const { type } = useParams()
+
+  if (type === undefined) {
+    console.log(1)
+  }
+
+  useEffect(() => {
+    if (type) {
+      const filteredProducts = allBikes.filter(product => product.type === type)
+      setProducts(filteredProducts)
+    } else {
+      setProducts(allBikes)
+    }
+  }, [type])
+
   return (
-    <section className='flex flex-col md:flex-col justify-around'>
-      <HeroImage image='all-bikes-image-01.jpg'>
+    <section className='flex flex-col md:flex-col justify-around bg-white'>
+      <HeroImage image='../src/assets/all-bikes-image-01.jpg'>
         <h2 className='text-2xl md:text-4xl mt-8'>Road Bikes</h2>
         <p className='mt-8 text-lg sm:text-xl md:text-3xl pr-16'>Designed for epic roads, dominating cobbles and pure speed against the clock, our bikes unlock your potential.</p>
       </HeroImage>
-      <article className='flex flex-row'>
+      <article className='flex flex-col md:flex-row py-8 justify-between w-full'>
         <Categories />
-        <ul className='p-8 flex flex-col gap-8'>
-          <li><h2>qqqqq</h2></li>
-          <li><h2>qqqqq</h2></li>
-          <li><h2>qqqqq</h2></li>
-        </ul>
+        <div className='w-full flex flex-col justify-center items-center'>
+          <Products products={products} layout='grid grid-cols-1 justify-center items-center xl:grid-cols-2 grid-flow-row gap-4 xl:gap-x-16' />
+        </div>
       </article>
     </section>
   )
 }
-
-/*
-
-import bikes from '../../src/mocks/bikes.json'
-import { Products } from '../components/Products'
-
-export function Bikes () {
-  const products = bikes.bikes
-  return (
-    <section className='p-8'>
-      <Products products={products} />
-      <h2>Bikes</h2>
-    </section>
-  )
-} */
