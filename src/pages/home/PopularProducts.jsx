21821b4e46fbde8b9b7c7eba0a6ad5561baf6dc9
Byron SeMo <byronsemo@gmail.com>
2023-11-05 18:24:57 -0700

    Change PopularProducts.jsx variable name

--- src/pages/home/PopularProducts.jsx
+++ src/pages/home/PopularProducts.jsx
@@ -1,14 +1,14 @@
 import { Products } from '../../components/Products'
-import bikes from '../../mocks/bikes.json'
+import products from '../../mocks/products.json'
 
 export function PopularProducts () {
-  const products = [bikes.bikes[9], bikes.bikes[10], bikes.bikes[3]]
+  const popularProducts = [products.bikes[9], products.bikes[10], products.bikes[3]]
 
   return (
     <section className='bg-white min-h-screen flex flex-col justify-center items-center'>
       <article className='p-4 pt-24 pb-8 flex flex-col justify-center items-center max-w-7xl'>
         <h2 className='text-5xl md:text-7xl text-slate-800'>Best Sellers</h2>
-        <Products products={products} layout='flex flex-col lg:flex-row py-10 gap-8 items-center justify-center' />
+        <Products products={popularProducts} layout='flex flex-col lg:flex-row py-10 gap-8 items-center justify-center' />
       </article>
     </section>
   )
