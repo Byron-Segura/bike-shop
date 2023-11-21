2f1f55fa55412660416c106aea1f36c5d7927b66
Byron SeMo <byronsemo@gmail.com>
2023-11-21 10:51:03 -0700

    Change Categories.jsx layout

--- src/pages/bikes/Categories.jsx
+++ src/pages/bikes/Categories.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { NavLink } from '../../components/NavLink'
 
-export function Categories () {
+export function Categories ({ categories }) {
   const [categorieIsActive, setCategorieIsActive] = useState(false)
 
   const handleClick = () => {
@@ -12,17 +12,21 @@ export function Categories () {
     window.scrollTo({ top: 0, behavior: 'smooth' })
   }
 
-  const categorieNavLinkStyle = categorieIsActive ? 'text-zinc-800 text-xl border-b-2 border-orange-500 lg:hover:border-b-2 lg:hover:border-orange-500 w-full py-4' : 'hidden'
+  const categorieNavLinkStyle = categorieIsActive ? 'text-zinc-800 text-xl border-b-2 border-orange-500 lg:hover:border-b-2 lg:hover:border-orange-500 w-full py-4 capitalize' : 'hidden'
 
   return (
     <article className='w-fit md:px-16 mb-8 mx-auto md:mx-0'>
       <ul className='flex flex-col mt-2 sticky top-8'>
-        <button onClick={handleClick} className='text-orange-500 text-xl text-center border-[1px] border-orange-500 py-2 px-20 sm:px-24 lg:hover:bg-orange-500 lg:hover:text-white'>Categories</button>
-        <NavLink to='road' styles={categorieNavLinkStyle} onClick={handleNavClick}>Road</NavLink>
-        <NavLink to='urban' styles={categorieNavLinkStyle} onClick={handleNavClick}>Urban</NavLink>
-        <NavLink to='gravel' styles={categorieNavLinkStyle} onClick={handleNavClick}>Gravel</NavLink>
-        <NavLink to='mountain' styles={categorieNavLinkStyle} onClick={handleNavClick}>Mountain</NavLink>
-        <NavLink to='active' styles={categorieNavLinkStyle} onClick={handleNavClick}>Active</NavLink>
+        <li>
+          <button onClick={handleClick} className='text-orange-500 text-xl text-center border-[1px] border-orange-500 py-2 px-20 sm:px-24 lg:hover:bg-orange-500 lg:hover:text-white'>Categories</button>
+        </li>
+        {categories.map(categorie => {
+          return (
+            <li key={categorie} className='flex flex-col'>
+              <NavLink to={categorie} styles={categorieNavLinkStyle} onClick={handleNavClick}>{categorie}</NavLink>
+            </li>
+          )
+        })}
       </ul>
     </article>
   )
